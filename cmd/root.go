116ffc8f3355f767110c74a9ce14bb665c55cf62
Immanuel Klinkenberg <drieschel@yahoo.de>
2025-10-04 20:30:16 +0200

    Cleanup

--- cmd/root.go
+++ cmd/root.go
@@ -11,14 +11,6 @@ var rootCmd = &cobra.Command{
 	Use:   "dddns",
 	Short: "A lightweight dyndns client",
 	Long:  `Drieschel's lightweight dyndns client`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//Run: func(cmd *cobra.Command, args []string) {
-	//	var clients []internal.Client
-	//	values := viper.UnmarshalKey("domain", &clients)
-	//
-	//	fmt.Printf("%+v\n", values)
-	//},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
